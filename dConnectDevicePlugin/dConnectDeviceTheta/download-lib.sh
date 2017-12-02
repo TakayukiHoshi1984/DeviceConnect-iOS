0b9f3b1870b7e273944e91e0b084f2167a97da18
TakayukiHoshi1984 <hoshi@gclue.jp>
2017-12-02 09:35:25 +0900

    Theta SDKのライブラリのソースコードをダウンロードするシェルを追加。

--- dConnectDevicePlugin/dConnectDeviceTheta/download-lib.sh
+++ dConnectDevicePlugin/dConnectDeviceTheta/download-lib.sh
@@ -7,33 +7,35 @@ D=$(cd $(dirname $0);pwd)
 THETA_R_EXIF="lib-r-exif"
 THETA_RICOH_THETA="lib-ricoh-theta"
 THETA_RICOH_THETA_SERIALIZER="lib-ricoh-theta_serializer"
-THETA_SDK=$D"/Classes/lib"
+THETA_SDK=$D"/dConnectDeviceTheta/Classes/lib"
 
-# GitHubからのダウンロード設定 TODO:Signature
-THETA_CORE_URL="https://developers.theta360.com/downloads?filename=RICOH_THETA_SDK_for_iOS.0.3.0.zip"
+THETA_CORE_URL="https://developers.theta360.com/ja/docs/sdk/download.html"
 THETA_MASTER="RICOH_THETA_SDK_for_iOS.0.3.0"
 THETA_ZIP_FILE=$THETA_MASTER".zip"
 # THETAのダウンロード確認
 if [ -d $THETA_SDK ]; then
     echo "THETA sdk is exist."
 else
-    # THETA sdkが存在しない場合には終了
+    # THETA sdkが存在しない場合にはダウンロードページから手動でダウンロードしてもらう
     echo "THETA sdk is not exist."
     echo ""
-    echo "THETASDKダウンロードページより、ダウンロード。"
-    echo "https://developers.theta360.com/downloads?filename=RICOH_THETA_SDK_for_iOS.0.3.0.zip"
+    echo "Please download SDK from THETA Developer page."
+    echo "https://developers.theta360.com/ja/docs/sdk/download.html"
 
     cd $D
 
     if [ -e $THETA_ZIP_FILE ]; then
         echo $THETA_FRAMEWORK" is exist."
     else
-        curl -o $THETA_ZIP_FILE -LOk $THETA_CORE_URL
+    	# Safariで開く
+		open -a safari $THETA_CORE_URL
     fi
 
-    unzip $THETA_ZIP_FILE
-
-    mv $D$"/"THETA_MASTER"/lib" $D"/Classes/"
-    rm -rf $D"/"$THETA_MASTER
-    rm -rf $D"/"$THETA_ZIP_FILE
+	read -p "Did you download Theta SDK? : ok?(Please press any key) :" yn
+	case "$yn" in
+	 *)
+		mv "$HOME/Downloads/"$THETA_MASTER"/lib" $THETA_SDK
+		rm -rf "$HOME/Downloads/"$THETA_MASTER
+	  ;;
+	esac
 fi
