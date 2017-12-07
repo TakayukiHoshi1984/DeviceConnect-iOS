a53203046ed1dc113affaa459a5f4b7805d3d172
TakayukiHoshi1984 <hoshi@gclue.jp>
2017-12-07 22:15:05 +0900

    Linkingのdownload.shの修正

--- dConnectDevicePlugin/dConnectDeviceLinking/download-framework.sh
+++ dConnectDevicePlugin/dConnectDeviceLinking/download-framework.sh
@@ -5,7 +5,7 @@ D=$(cd $(dirname $0);pwd)
 
 # LinkingのSDKへのパス
 LINKING_FRAMEWORK="LinkingLibrary.framework"
-LINKING_SDK=$D"/Libs/Release/"$Linking_FRAMEWORK
+LINKING_SDK=$D"/Libs/Release/"$LINKING_FRAMEWORK
 
 # GitHubからのダウンロード設定
 LINKING_CORE_URL="https://linkingiot.com/developer/zip/LinkingiOS_SDK.zip"
@@ -33,8 +33,8 @@ else
     unzip $LINKING_ZIP_FILE
     unzip $LINKING_MASTER"/"$LINKING_RELEASE_ZIP_FILE
 
-    mv $D"/"$LINKING_MASTER"/Release/Release/"$LINKING_FRAMEWORK $D"/Libs/Release"
-    rm -rf $D"/"$LINKING_MASTER
-    rm -rf $D"/Release"
-    rm -rf $D"/"$LINKING_ZIP_FILE
+    mv $D"/Release/Release/"$LINKING_FRAMEWORK $D"/Libs/Release"
+#     rm -rf $D"/"$LINKING_MASTER
+#     rm -rf $D"/Release"
+#     rm -rf $D"/"$LINKING_ZIP_FILE
 fi
